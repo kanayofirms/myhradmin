ce315bdd0346090b8f1b6e376da4d42f7cbd5edc
kanayofirms <kanyeze@gmail.com>
2024-06-04 15:41:56 +0100

    Create GET route for register page

--- routes/web.php
+++ routes/web.php
@@ -15,3 +15,5 @@ use App\Http\Controllers\AuthController;
 */
 
 Route::get('/', [AuthController::class, 'index']);
+Route::get('forgot-password', [AuthController::class, 'forgot_password']);
+Route::get('register', [AuthController::class, 'register']);
