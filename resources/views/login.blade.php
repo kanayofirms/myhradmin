80d9ef201e57fbfa1f69128dbe567a97d610a973
kanayofirms <kanyeze@gmail.com>
2024-06-04 15:42:43 +0100

    Update login page for feasibility and update app

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -59,10 +59,10 @@
       <!-- /.social-auth-links -->
 
       <p class="mb-1">
-        <a href="forgot-password.html">I forgot my password</a>
+        <a href="{{ url('forgot-password') }}">I forgot my password</a>
       </p>
       <p class="mb-0">
-        <a href="register.html" class="text-center">Register a new membership</a>
+        <a href="{{ url('register') }}" class="text-center">Register a new membership</a>
       </p>
     </div>
     <!-- /.login-card-body -->
