26d0c9f157b45692dd1c42f7ad3cdd97b1419fa0
kanayofirms <kanyeze@gmail.com>
2024-06-05 07:02:46 +0100

    Add validation to register post function and update app

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -5,8 +5,10 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
+use Str;
 
-class AuthController extends Controller{
+class AuthController extends Controller
+{
     public function index(Request $request)
     {
         return view('login');
@@ -25,14 +27,21 @@ class AuthController extends Controller{
     public function register_post(Request $request)
     {
         // dd($request->all());
-        $user = new User;
-        $user->name = trim($request->name);
-        $user->email = trim($request->email);
-        $user->password = Hash::make($request->password);
+        $user = request()->validate([
+            'name'             => 'required',
+            'email'            => 'required|unique:users',
+            'password'         => 'required|min:6',
+            'confirm_password' => 'required_with:password|same:password|min:6'
+        ]);
+
+
+        $user                  = new User;
+        $user->name            = trim($request->name);
+        $user->email           = trim($request->email);
+        $user->password        = Hash::make($request->password);
+        $user->remember_token  = Str::random(50);
         $user->save();
 
         return redirect('/')->with('success', 'Register Successfully.');
     }
 }
-
-?>
