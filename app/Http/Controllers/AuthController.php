94949a43542d2711398090205ea82618aa418fce
kanayofirms <kanyeze@gmail.com>
2024-06-05 05:00:20 +0100

    Create function to post register data to the database

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,7 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller{
     public function index(Request $request)
@@ -19,6 +20,18 @@ class AuthController extends Controller{
     public function register(Request $request)
     {
         return view('register');
+    }
+
+    public function register_post(Request $request)
+    {
+        // dd($request->all());
+        $user = new User;
+        $user->name = trim($request->name);
+        $user->email = trim($request->email);
+        $user->password = Hash::make($request->password);
+        $user->save();
+
+        return redirect('/')->with('success', 'Register Successfully.');
     }
 }
 
