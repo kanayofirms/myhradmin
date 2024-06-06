431042baeddaa0beb84c9bbb2daa5958887ac194
kanayofirms <kanyeze@gmail.com>
2024-06-06 10:55:11 +0100

    Refactor login post function to show error message in validation

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -59,7 +59,7 @@ class AuthController extends Controller
 
     public function login_post(Request $request)
     {
-        if(Auth::attempt(['email' => $request->email, 'password' => $request->password, true])) {
+        if(Auth::attempt(['email' => $request->email, 'password' => $request->password], true)) {
             if(Auth::User()->is_role == "1") {
                 return redirect()->intended('admin/dashboard');
             } else {
