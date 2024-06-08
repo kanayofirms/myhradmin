911379d3f055af9bf671fc03d3f5a0d317b856cd
kanayofirms <kanyeze@gmail.com>
2024-06-08 15:08:33 +0100

    Create GET route for add employees on admin side

--- routes/web.php
+++ routes/web.php
@@ -33,6 +33,8 @@ Route::group(['middleware' => 'admin'], function(){
     Route::get('admin/dashboard', [DashboardController::class, 'dashboard']);
 
     Route::get('admin/employees', [EmployeesController::class, 'index']);
+    Route::get('admin/employees/add', [EmployeesController::class, 'add']);
+
 });
 
 Route::get('logout', [AuthController::class, 'logout']);
