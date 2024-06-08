b6f13571b5cb2e925c4a4f46c0018f7b006c4e28
kanayofirms <kanyeze@gmail.com>
2024-06-08 11:13:44 +0100

    Fetch user name from db and make dynamic

--- resources/views/backend/layouts/_sidebar.blade.php
+++ resources/views/backend/layouts/_sidebar.blade.php
@@ -11,10 +11,7 @@
         <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
       </li>
       <li class="nav-item d-none d-sm-inline-block">
-        <a href="index3.html" class="nav-link">Home</a>
-      </li>
-      <li class="nav-item d-none d-sm-inline-block">
-        <a href="#" class="nav-link">Contact</a>
+        <a href="" class="nav-link">Home</a>
       </li>
     </ul>
 
@@ -22,8 +19,8 @@
     <ul class="navbar-nav ml-auto">
       <!-- Navbar Search -->
       <li class="nav-item">
-        <a class="nav-link" data-widget="navbar-search" href="#" role="button">
-          <i class="fas fa-search"></i>
+        <a class="nav-link" href="{{ url('logout') }}">
+          <i class="fas fa-sign-out-alt"></i>
         </a>
        
       </li>
@@ -48,7 +45,7 @@
           <img src="{{ url('public/backend/dist/img/user2-160x160.jpg') }}" class="img-circle elevation-2" alt="User Image">
         </div>
         <div class="info">
-          <a href="#" class="d-block">Alexander Pierce</a>
+          <a href="" class="d-block">{{ Auth::user()->name }}</a>
         </div>
       </div>
 
@@ -60,8 +57,8 @@
                with font-awesome or any other icon font library -->    
            
           <li class="nav-item">
-            <a href="pages/gallery.html" class="nav-link active">
-              <i class="nav-icon far fa-dashboard"></i>
+            <a href="{{ url('admin/dashboard') }}" class="nav-link active">
+              <i class="nav-icon fa fa-home"></i>
               <p>
                 Dashboard
               </p>
