6f5852fa45d0fe1744dcbd7d6ff75e0e6559872a
kanayofirms <kanyeze@gmail.com>
2024-06-13 00:00:05 +0100

    Refactor validation message for readability

--- resources/views/_message.blade.php
+++ resources/views/_message.blade.php
@@ -1,15 +1,13 @@
 
 
-@if (!@empty(session('success')))
+@if (!empty(session('success')))
     <div class="alert alert-success" role="alert">
         {{ session('success') }}
     </div>
-    
 @endif
 
-@if (!@empty(session('error')))
+@if (!empty(session('error')))
     <div class="alert alert-danger" role="alert">
         {{ session('error') }}
     </div>
-    
 @endif
