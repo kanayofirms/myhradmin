6fd329a67ad5ffba5cca53984d8812b580bfc30d
kanayofirms <kanyeze@gmail.com>
2024-06-19 14:40:57 +0100

    Create list for employees data

--- resources/views/backend/employees/view.blade.php
+++ resources/views/backend/employees/view.blade.php
@@ -1,1 +1,70 @@
-hello world!
+@extends('backend.layouts.app')
+
+@section('content')
+    <!-- Content Wrapper. Contains page content -->
+    <div class="content-wrapper">
+        <!-- Content Header (Page header) -->
+        <div class="content-header">
+            <div class="container-fluid">
+                <div class="row mb-2">
+                    <div class="col-sm-6">
+                        <h1 class="m-0">View Employees</h1>
+                    </div><!-- /.col -->
+                    <div class="col-sm-6">
+                        <ol class="breadcrumb float-sm-right">
+                            <li class="breadcrumb-item"><a href="#">Add</a></li>
+                            <li class="breadcrumb-item active">Employees</li>
+                        </ol>
+                    </div><!-- /.col -->
+                </div><!-- /.row -->
+            </div><!-- /.container-fluid -->
+        </div>
+        <!-- /.content-header -->
+
+        <section class="content">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="card card-info">
+                            <div class="card-header">
+                                <h3 class="card-title">View Employees</h3>
+                            </div>
+
+                            <form class="form-horizontal" method="post"
+                                enctype="multipart/form-data">
+
+                                <div class="card card-body">
+
+                                    <div class="form-group row">
+                                        <label class="col-sm-2 col-form-label">ID
+                                            <span style="color: red;">*</span></label>
+                                        <div class="col-sm-10">
+                                            {{ $getRecord->id }}
+                                        </div>
+                                    </div>
+
+                                    <div class="form-group row">
+                                        <label class="col-sm-2 col-form-label">First Name
+                                            <span style="color: red;">*</span></label>
+                                        <div class="col-sm-10">
+                                            {{ $getRecord->name }}
+                                        </div>
+                                    </div>
+
+                                </div>
+
+                                <div class="card-footer">
+                                    <a href="{{ url('admin/employees') }}" class="btn btn-default">Back</a>
+
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+         <!-- /.content -->
+    </div>
+    <!-- /.content-wrapper -->
+@endsection
