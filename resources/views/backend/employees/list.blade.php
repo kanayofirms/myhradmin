930e08ba50ee9424b46ec0fd958c1fb1dd70bb37
kanayofirms <kanyeze@gmail.com>
2024-06-19 04:56:21 +0100

    Refactor view to dynamically fetch data from the db

--- resources/views/backend/employees/list.blade.php
+++ resources/views/backend/employees/list.blade.php
@@ -1,65 +1,102 @@
 @extends('backend.layouts.app')
 
-@section('content') 
+@section('content')
+    <!-- Content Wrapper. Contains page content -->
+    <div class="content-wrapper">
+        <!-- Content Header (Page header) -->
+        <div class="content-header">
+            <div class="container-fluid">
+                <div class="row mb-2">
+                    <div class="col-sm-6">
+                        <h1>Employees</h1>
+                    </div><!-- /.col -->
+                    <div class="col-sm-6" style="text-align:right;">
+                        <a href="{{ url('admin/employees/add') }}" class="btn btn-primary">
+                            Add Employees
+                        </a>
+                    </div><!-- /.col -->
+                </div><!-- /.row -->
+            </div><!-- /.container-fluid -->
+        </div>
+        <!-- /.content-header -->
 
-  <!-- Content Wrapper. Contains page content -->
-  <div class="content-wrapper">
-    <!-- Content Header (Page header) -->
-    <div class="content-header">
-      <div class="container-fluid">
-        <div class="row mb-2">
-          <div class="col-sm-6">
-            <h1>Employees</h1>
-          </div><!-- /.col -->
-          <div class="col-sm-6" style="text-align:right;">
-            <a href="{{ url('admin/employees/add') }}" class="btn btn-primary">
-              Add Employees
-            </a>
-          </div><!-- /.col -->
-        </div><!-- /.row -->
-      </div><!-- /.container-fluid -->
-    </div>
-    <!-- /.content-header -->
-    {{-- @include('_message') --}}
+        <section class="content">
+            <div class="container-fluid">
 
-    <section class="content">
-      <div class="container-fluid">
-          
-            <div class="row">
-                <section class="col-md-12">
-                  <div class="card">
-                    <div class="card-header">
+                <div class="row">
+                    <section class="col-md-12">
+                        <div class="card">
+                            <div class="card-header">
 
-                      <h3 class="card-title">Search Employees</h3>
-                      
-                    </div>
+                                <h3 class="card-title">Search Employees</h3>
 
-                    <form action="" method="get">
-                      <div class="card-body">
-                        <div class="row">
+                            </div>
 
-                          <div class="form-group col-md-3">
-                            <label for="">First Name</label>
-                            <input type="text" name="" class="form-control" placeholder="First Name">
-                          </div>
+                            <form action="" method="get">
+                                <div class="card-body">
+                                    <div class="row">
 
-                          <div class="form-group col-md-3">
-                            <label for="">Surname</label>
-                            <input type="text" name="" class="form-control" placeholder="Surname">
-                          </div>
+                                        <div class="form-group col-md-3">
+                                            <label for="">First Name</label>
+                                            <input type="text" name="" class="form-control"
+                                                placeholder="First Name">
+                                        </div>
+
+                                        <div class="form-group col-md-3">
+                                            <label for="">Surname</label>
+                                            <input type="text" name="" class="form-control" placeholder="Surname">
+                                        </div>
+
+                                        <div class="form-group col-md-3">
+                                            <button class="btn btn-primary" type="submit"
+                                                style="margin-top: 30px;">Search</button>
+                                            <a href="{{ url('admin/employees') }}" class="btn btn-success"
+                                                style="margin-top: 30px;">Reset</a>
+                                        </div>
+
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+
+                        @include('_message')
+
+                        <div class="card">
+                            <div class="card-header">
+                                <h3 class="card-title">Employees List</h3>
+                            </div>
+
+                            <div class="card-body p-0">
+                                <table class="table table-striped">
+                                    <thead>
+                                        <tr>
+                                            <th>#</th>
+                                            <th>First Name</th>
+                                            <th>Surname</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        @foreach ($getRecord as $value)
+                                        <tr>
+                                            <td>{{ $value->id }}</td>
+                                            <td>{{ $value->name }}</td>
+                                            <td>{{ $value->last_name }}</td>
+                                        </tr>
+                                        @endforeach
+                                    </tbody>
+                                </table>
+                                <div style="padding:10px; float:right;">
+                                    {!! $getRecord->appends(Illuminate\Support\Facades\Request::except('page'))->links() !!}
+                                </div>
+                            </div>
 
                         </div>
-                      </div>
-                    </form>
-                  </div>
-                </section>
+                    </section>
+                </div>
             </div>
-        </div>
-    </section>
+        </section>
 
-    <!-- /.content -->
-  </div>
-  <!-- /.content-wrapper -->
-
-
-  @endsection
+        <!-- /.content -->
+    </div>
+    <!-- /.content-wrapper -->
+@endsection
