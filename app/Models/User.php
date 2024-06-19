2f6f6b89aeefa00e7db287e444afe4d08d85dad4
kanayofirms <kanyeze@gmail.com>
2024-06-19 10:41:31 +0100

    Create search query for users

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Request;
 
 class User extends Authenticatable
 {
@@ -43,11 +44,38 @@ class User extends Authenticatable
     ];
 
     static public function getRecord(){
-        $return = self::select('users.*')
-                ->orderBy('id', 'desc')
-                ->paginate(5);
+        // $return = self::select('users.*')
+        //         ->orderBy('id', 'desc')
+        //         ->paginate(20);
 
-                return $return;
+        //         return $return;
+        $return = self::select('users.*');
+
+                //search box start
+                    if(!empty(Request::get('id')))
+                    {
+                        $return = $return->where('id', Request::get('id'));
+                    }
+
+                    if(!empty(Request::get('name')))
+                    {
+                        $return = $return->where('name', 'LIKE', '%' .Request::get('name'). '%');
+                    }
+
+                    if(!empty(Request::get('last_name')))
+                    {
+                        $return = $return->where('last_name', 'LIKE', '%'.Request::get('last_name').'%');
+                    }
+
+                    if(!empty(Request::get('email')))
+                    {
+                        $return = $return->where('email', 'LIKE', '%'.Request::get('email'). '%');
+                    }
+                //search box end
+
+                $return = $return->orderBy('id','desc')
+                ->paginate(20);
+        return $return;
     }
 
 }
