0ec9a37930fb153c1c809cc4d013df8f1cea4333
kanayofirms <kanyeze@gmail.com>
2024-06-19 19:12:23 +0100

    Create POST request to update user record and update app

--- routes/web.php
+++ routes/web.php
@@ -37,6 +37,7 @@ Route::group(['middleware' => 'admin'], function(){
     Route::post('admin/employees/add', [EmployeesController::class, 'add_insert']);
     Route::get('admin/employees/view/{id}', [EmployeesController::class, 'view']);
     Route::get('admin/employees/edit/{id}', [EmployeesController::class, 'edit']);
+    Route::post('admin/employees/edit/{id}', [EmployeesController::class, 'edit_update']);
 
 });
 
