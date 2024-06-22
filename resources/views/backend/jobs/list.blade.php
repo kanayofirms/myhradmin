02bcd82ee19056319a6dc7757c373db0e71b1dd8
kanayofirms <kanyeze@gmail.com>
2024-06-23 00:21:58 +0100

    Refactor code and update app

---
+++ resources/views/backend/jobs/list.blade.php
@@ -0,0 +1,131 @@
+@extends('backend.layouts.app')
+
+@section('content')
+    <!-- Content Wrapper. Contains page content -->
+    <div class="content-wrapper">
+        <!-- Content Header (Page header) -->
+        <div class="content-header">
+            <div class="container-fluid">
+                <div class="row mb-2">
+                    <div class="col-sm-6">
+                        <h1>Jobs</h1>
+                    </div><!-- /.col -->
+                    <div class="col-sm-6" style="text-align:right;">
+                        <a href="{{ url('admin/jobs/add') }}" class="btn btn-primary">
+                            Add Jobs
+                        </a>
+                    </div><!-- /.col -->
+                </div><!-- /.row -->
+            </div><!-- /.container-fluid -->
+        </div>
+        <!-- /.content-header -->
+
+        <section class="content">
+            <div class="container-fluid">
+
+                <div class="row">
+                    <section class="col-md-12">
+                        <div class="card">
+                            <div class="card-header">
+
+                                <h3 class="card-title">Search Jobs</h3>
+
+                            </div>
+
+                            <form action="" method="get">
+                                <div class="card-body">
+                                    <div class="row">
+
+                                        <div class="form-group col-md-1">
+                                            <label for="">ID</label>
+                                            <input type="text" name="id" class="form-control"
+                                                value="{{ Request()->id }}" placeholder="ID">
+                                        </div>
+
+                                        <div class="form-group col-md-3">
+                                            <label for="">Job Title</label>
+                                            <input type="text" value="{{ Request()->job_title }}" name="job_title"
+                                                class="form-control" placeholder="Job Title">
+                                        </div>
+
+                                        <div class="form-group col-md-3">
+                                            <label for="">Min Salary</label>
+                                            <input type="text" value="{{ Request()->min_salary }}" name="min_salary"
+                                                class="form-control" placeholder="Min Salary">
+                                        </div>
+
+                                        <div class="form-group col-md-3">
+                                            <label for="">Max Salary</label>
+                                            <input type="text" value="{{ Request()->max_salary }}" name="max_salary"
+                                                class="form-control" placeholder="Max Salary">
+                                        </div>
+
+                                        <div class="form-group col-md-2">
+                                            <button class="btn btn-primary" type="submit"
+                                                style="margin-top: 30px;">Search</button>
+                                            <a href="{{ url('admin/jobs') }}" class="btn btn-success"
+                                                style="margin-top: 30px;">Reset</a>
+                                        </div>
+
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+
+                        @include('_message')
+
+                        <div class="card">
+                            <div class="card-header">
+                                <h3 class="card-title">Jobs List</h3>
+                            </div>
+
+                            <div class="card-body p-0">
+                                <table class="table table-striped">
+                                    <thead>
+                                        <tr>
+                                            <th>ID</th>
+                                            <th>Job Title</th>
+                                            <th>Min Salary</th>
+                                            <th>Max Salary</th>
+                                            <th>Created At</th>
+                                            <th>Action</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        @forelse ($getRecord as $value)
+                                            <tr>
+                                                <td>{{ $value->id }}</td>
+                                                <td>{{ $value->job_title }}</td>
+                                                <td>{{ $value->min_salary }}</td>
+                                                <td>{{ $value->max_salary }}</td>
+                                                <td>{{ date('d-m-Y', strtotime($value->updated_at)) }}</td>
+                                                <td>
+                                                    <a href="{{ url('admin/jobs/view/'.$value->id) }}" class="btn btn-info">View</a>
+                                                    <a href="" class="btn btn-primary">Edit</a>
+                                                    <a href=""
+                                                        onclick="return confirm('Are you sure you want to delete?')"
+                                                        class="btn btn-danger">Delete</a>
+                                                </td>
+                                            </tr>
+                                        @empty
+                                            <tr>
+                                                <td colspan="100%">No Record Found.</td>
+                                            </tr>
+                                        @endforelse
+                                    </tbody>
+                                </table>
+                                <div style="padding:10px; float:right;">
+
+                                </div>
+                            </div>
+
+                        </div>
+                    </section>
+                </div>
+            </div>
+        </section>
+
+        <!-- /.content -->
+    </div>
+    <!-- /.content-wrapper -->
+@endsection
