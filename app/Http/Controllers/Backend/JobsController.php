e3630ffe7db56c57e0d1f27807b22fa550333682
kanayofirms <kanyeze@gmail.com>
2024-06-23 08:27:06 +0100

    Import jobs model and update app

--- app/Http/Controllers/Backend/JobsController.php
+++ app/Http/Controllers/Backend/JobsController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Backend;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\User;
+use App\Models\JobsModel;
 
 
 class JobsController extends Controller
