3a60d98d3111d48bb96fde943114650057d4161e
kanayofirms <kanyeze@gmail.com>
2024-06-25 13:07:41 +0100

    Create get record function and search queries

--- app/Models/JobsModel.php
+++ app/Models/JobsModel.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Request;
 
 class JobsModel extends Model
 {
@@ -11,5 +12,41 @@ class JobsModel extends Model
 
     protected $table = 'jobs';
 
-    
+    static public function getRecord()
+    {
+        // $return = self::select('jobs.*')
+        //         ->orderBy('id', 'desc')
+        //         ->paginate(20);
+
+        //         return $return;
+        $return = self::select('jobs.*');
+
+                //search box start
+                    if(!empty(Request::get('id')))
+                    {
+                        $return = $return->where('id', Request::get('id'));
+                    }
+
+                    if(!empty(Request::get('job_title')))
+                    {
+                        $return = $return->where('job_title', 'LIKE', '%'.Request::get('job_title').'%');
+                    }
+
+                    if(!empty(Request::get('min_salary')))
+                    {
+                        $return = $return->where('min_salary', 'LIKE', '%'.Request::get('min_salary').'%');
+                    }
+
+                    if(!empty(Request::get('max_salary')))
+                    {
+                        $return = $return->where('max_salary', 'LIKE', '%'.Request::get('max_salary').'%');
+                    }
+                //search box end
+
+                $return = $return->orderBy('id','desc')
+                ->paginate(20);
+        return $return;
+    }
+
+
 }
