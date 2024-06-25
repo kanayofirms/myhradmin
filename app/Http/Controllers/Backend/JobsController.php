941bedf29a94fa5da94f91366f009723a1664694
kanayofirms <kanyeze@gmail.com>
2024-06-25 12:05:54 +0100

    Create validation and insert job record to the db

--- app/Http/Controllers/Backend/JobsController.php
+++ app/Http/Controllers/Backend/JobsController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers\Backend;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use App\Models\User;
 use App\Models\JobsModel;
 
 
@@ -17,5 +16,24 @@ class JobsController extends Controller
     public function add()
     {
         return view('backend.jobs.add');
+    }
+
+    public function add_post(Request $request)
+    {
+        // dd($request->all());
+
+        $user = request()->validate([
+            'job_title' => 'required',
+            'min_salary' => 'required',
+            'max_salary' => 'required',
+        ]);
+        $user                   = new JobsModel;
+        $user->job_title             = trim($request->job_title);
+        $user->min_salary        = trim($request->min_salary);
+        $user->max_salary            = trim($request->max_salary);
+
+        $user->save();
+
+        return redirect('admin/jobs')->with('success', 'Jobs successfully registered.');
     }
 }
