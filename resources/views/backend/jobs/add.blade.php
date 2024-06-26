08647467a0090b55fa083876bfd19b353a4f6b0a
kanayofirms <kanyeze@gmail.com>
2024-06-26 07:06:20 +0100

    Refactor code for readability

--- resources/views/backend/jobs/add.blade.php
+++ resources/views/backend/jobs/add.blade.php
@@ -8,12 +8,12 @@
             <div class="container-fluid">
                 <div class="row mb-2">
                     <div class="col-sm-6">
-                        <h1 class="m-0">Employees</h1>
+                        <h1 class="m-0">Add Jobs</h1>
                     </div><!-- /.col -->
                     <div class="col-sm-6">
                         <ol class="breadcrumb float-sm-right">
                             <li class="breadcrumb-item"><a href="#">Add</a></li>
-                            <li class="breadcrumb-item active">Employees</li>
+                            <li class="breadcrumb-item active">Jobs</li>
                         </ol>
                     </div><!-- /.col -->
                 </div><!-- /.row -->
@@ -64,7 +64,7 @@
                                 </div>
 
                                 <div class="card-footer">
-                                    <a href="{{ url('admin/employees') }}" class="btn btn-default">Back</a>
+                                    <a href="{{ url('admin/jobs') }}" class="btn btn-default">Back</a>
                                     <button type="submit" class="btn btn-primary float-right">Submit</button>
                                 </div>
                             </form>
