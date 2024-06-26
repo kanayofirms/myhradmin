519085c4392674dff955ecc0780f938799b995db
kanayofirms <kanyeze@gmail.com>
2024-06-26 23:38:37 +0100

    Refactor get record function with query for start date and end date

--- app/Models/JobsModel.php
+++ app/Models/JobsModel.php
@@ -21,30 +21,31 @@ class JobsModel extends Model
         //         return $return;
         $return = self::select('jobs.*');
 
-                //search box start
-                    if(!empty(Request::get('id')))
-                    {
-                        $return = $return->where('id', Request::get('id'));
-                    }
+        //search box start
+        if (!empty(Request::get('id'))) {
+            $return = $return->where('id', Request::get('id'));
+        }
 
-                    if(!empty(Request::get('job_title')))
-                    {
-                        $return = $return->where('job_title', 'LIKE', '%'.Request::get('job_title').'%');
-                    }
+        if (!empty(Request::get('job_title'))) {
+            $return = $return->where('job_title', 'LIKE', '%' . Request::get('job_title') . '%');
+        }
 
-                    if(!empty(Request::get('min_salary')))
-                    {
-                        $return = $return->where('min_salary', 'LIKE', '%'.Request::get('min_salary').'%');
-                    }
+        if (!empty(Request::get('min_salary'))) {
+            $return = $return->where('min_salary', 'LIKE', '%' . Request::get('min_salary') . '%');
+        }
 
-                    if(!empty(Request::get('max_salary')))
-                    {
-                        $return = $return->where('max_salary', 'LIKE', '%'.Request::get('max_salary').'%');
-                    }
-                //search box end
+        if (!empty(Request::get('max_salary'))) {
+            $return = $return->where('max_salary', 'LIKE', '%' . Request::get('max_salary') . '%');
+        }
 
-                $return = $return->orderBy('id','desc')
-                ->paginate(20);
+        if (!empty(Request::get('start_date')) && !empty(Request::get('end_date'))) {
+            $return = $return->where('jobs.created_at', '>=', Request::get('start_date'))
+            ->where('jobs.created_at', '<=', Request::get('end_date'));
+        }
+        //search box end
+
+        $return = $return->orderBy('id', 'desc')
+            ->paginate(20);
         return $return;
     }
 
