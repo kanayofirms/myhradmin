37b5bb242f145fc76594499a7dbe4497bccc3f77
kanayofirms <kanyeze@gmail.com>
2024-06-29 22:22:03 +0100

    Create add post function to insert data into db with validations

--- app/Http/Controllers/Backend/JobHistoryController.php
+++ app/Http/Controllers/Backend/JobHistoryController.php
@@ -23,4 +23,26 @@ class JobHistoryController extends Controller
         $data['getJobs'] = JobsModel::get();
         return view('backend.job_history.add', $data);
     }
+
+    public function add_post(Request $request)
+    {
+        // dd($request->all());
+        $user = request()->validate([
+            'employee_id'   => 'required',
+            'start_date'    => 'required',
+            'end_date'      => 'required',
+            'job_id'        => 'required',
+            'department_id' => 'required'
+        ]);
+
+        $user                   =   new JobHistoryModel;
+        $user->employee_id      =   trim($request->employee_id);
+        $user->start_date       =   trim($request->start_date);
+        $user->end_date         =   trim($request->end_date);
+        $user->job_id           =   trim($request->job_id);
+        $user->department_id    =   trim($request->department_id);
+        $user->save();
+
+        return redirect('admin/job_history')->with('success','Job History successfully added.');
+    }
 }
