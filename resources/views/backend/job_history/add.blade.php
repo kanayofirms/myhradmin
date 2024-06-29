51919bce4b816b101bc7f802a0cfe2dcab8433a4
kanayofirms <kanyeze@gmail.com>
2024-06-29 17:32:32 +0100

    Create fields for job history

---
+++ resources/views/backend/job_history/add.blade.php
@@ -0,0 +1,112 @@
+@extends('backend.layouts.app')
+
+@section('content')
+    <!-- Content Wrapper. Contains page content -->
+    <div class="content-wrapper">
+        <!-- Content Header (Page header) -->
+        <div class="content-header">
+            <div class="container-fluid">
+                <div class="row mb-2">
+                    <div class="col-sm-6">
+                        <h1 class="m-0">Job History</h1>
+                    </div><!-- /.col -->
+                    <div class="col-sm-6">
+                        <ol class="breadcrumb float-sm-right">
+                            <li class="breadcrumb-item"><a href="#">Add</a></li>
+                            <li class="breadcrumb-item active">Job History</li>
+                        </ol>
+                    </div><!-- /.col -->
+                </div><!-- /.row -->
+            </div><!-- /.container-fluid -->
+        </div>
+        <!-- /.content-header -->
+
+        <section class="content">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="card card-info">
+                            <div class="card-header">
+                                <h3 class="card-title">Add History</h3>
+                            </div>
+
+                            <form action="{{ url('admin/job_history/add') }}" class="form-horizontal" method="post"
+                                enctype="multipart/form-data">
+                                {{ csrf_field() }}
+                                <div class="card card-body">
+                                    <div class="form-group row">
+                                        <label class="col-sm-2 col-form-label">Employee Name
+                                            <span style="color: red;">*</span></label>
+                                        <div class="col-sm-10">
+                                           <select name="employee_id" id="" class="form-control">
+                                            <option value="">Employees</option>
+                                           </select>
+                                        </div>
+                                    </div>
+
+                                    <div class="form-group row">
+                                        <label class="col-sm-2 col-form-label">Start Date
+                                            <span style="color: red;">*</span></label>
+                                        <div class="col-sm-10">
+                                            <input type="date" value="{{ old('start_date') }}" name="start_date"
+                                                class="form-control" required>
+                                        </div>
+                                    </div>
+
+                                    <div class="form-group row">
+                                        <label class="col-sm-2 col-form-label">End Date
+                                            <span style="color: red;">*</span></label>
+                                        <div class="col-sm-10">
+                                            <input type="date" value="{{ old('end_date') }}" name="end_date"
+                                                class="form-control" required>
+                                        </div>
+                                    </div>
+
+                                    <div class="form-group row">
+                                        <label class="col-sm-2 col-form-label">Job Name (Job ID)
+                                            <span style="color: red;">*</span></label>
+                                        <div class="col-sm-10">
+                                           <select name="job_id" id="" class="form-control">
+                                            <option value="">Employees</option>
+                                           </select>
+                                        </div>
+                                    </div>
+
+                                    <div class="form-group row">
+                                        <label class="col-sm-2 col-form-label">Job Name (Job ID)
+                                            <span style="color: red;">*</span></label>
+                                        <div class="col-sm-10">
+                                           <select name="job_id" id="" class="form-control">
+                                            <option value="">Employees</option>
+                                           </select>
+                                        </div>
+                                    </div>
+
+                                    <div class="form-group row">
+                                        <label class="col-sm-2 col-form-label">Department Name (Department ID)
+                                            <span style="color: red;">*</span></label>
+                                        <div class="col-sm-10">
+                                           <select name="department_id" id="" class="form-control">
+                                            <option value="">Employees</option>
+                                           </select>
+                                        </div>
+                                    </div>
+
+                                </div>
+
+                                <div class="card-footer">
+                                    <a href="{{ url('admin/job_history') }}" class="btn btn-default">Back</a>
+                                    <button type="submit" class="btn btn-primary float-right">Submit</button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <!-- /.content -->
+    </div>
+    <!-- /.content-wrapper -->
+@endsection
+
