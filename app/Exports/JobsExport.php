0261eca570fadfc7d8373fa7233b6809782d6673
kanayofirms <kanyeze@gmail.com>
2024-06-29 15:00:17 +0100

    Create tables and dynamic set values

--- app/Exports/JobsExport.php
+++ app/Exports/JobsExport.php
@@ -17,14 +17,25 @@ class JobsExport implements FromCollection, WithMapping, ShouldAutoSize, WithHea
     protected $index = 0;
 
     public function map($user): array {
+        $createdAtFormat = date('d-m-Y', strtotime($user->created_at));
         return [
             ++ $this->index,
+            $user->id,
+            $user->job_title,
+            $user->min_salary,
+            $user->max_salary,
+            $createdAtFormat
         ];
         }
 
         public function headings(): array {
             return [
                 'S.No',
+                'Table ID',
+                'Job Title',
+                'Min Salary',
+                'Max Salary',
+                'Created At',
             ];
         }
 
