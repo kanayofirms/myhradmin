939babb6aaf13e80de0e9899942ef1736058cf1c
kanayofirms <kanyeze@gmail.com>
2024-06-29 17:30:37 +0100

    Create add function for job history

--- app/Http/Controllers/Backend/JobHistoryController.php
+++ app/Http/Controllers/Backend/JobHistoryController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers\Backend;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Models\JobsModel;
+use App\Models\JobHistoryModel;
+
+
 
 
 class JobHistoryController extends Controller
@@ -11,5 +14,10 @@ class JobHistoryController extends Controller
     public function index(Request $request)
     {
         return view('backend.job_history.list');
+    }
+
+    public function add()
+    {
+        return view('backend.job_history.add');
     }
 }
