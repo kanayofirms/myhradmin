5be55f14ad075808d62d29f65f52b82306816caf
kanayofirms <kanyeze@gmail.com>
2024-06-30 10:13:06 +0100

    Create simple and advanced queries on getRecord function

--- app/Models/JobHistoryModel.php
+++ app/Models/JobHistoryModel.php
@@ -15,9 +15,46 @@ class JobHistoryModel extends Model
 
     static public function getRecord($request)
     {
-        $return = self::select('job_history.*')
-            ->orderBy('id', 'desc')
-            ->paginate(20);
+        // $return = self::select('job_history.*')
+        //     ->orderBy('id', 'desc')
+        //     ->paginate(20);
+        // return $return;
+
+        $return = self::select('job_history.*', 'users.name', 'jobs.job_title')
+                ->join('users','users.id','=','job_history.employee_id')
+                ->join('jobs','jobs.id','=','job_history.job_id')
+                ->orderBy('job_history.id','desc');
+
+                if(!empty(Request::get('id')))
+                {
+                    $return = $return->where('job_history.id', Request::get('id'));
+                }
+
+                if(!empty(Request::get('name')))
+                {
+                    $return = $return->where('users.name', 'like', '%'.Request::get('name').'%');
+                }
+
+                if(!empty(Request::get('start_date')))
+                {
+                    $return = $return->where('job_history.start_date', Request::get(
+                        'start_date'));
+
+                }
+
+                if(!empty(Request::get('end_date')))
+                {
+                    $return = $return->where('job_history.start_date', Request::get(
+                        'end_date'));
+
+                }
+
+                if(!empty(Request::get('job_title')))
+                {
+                    $return = $return->where('jobs.job_title', 'like', '%'.Request::get('job_title').'%');
+                }
+
+                $return = $return->paginate(20);
         return $return;
     }
 
