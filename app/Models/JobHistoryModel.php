9d105600983ddea5081c05f926076c2ce3fcd169
kanayofirms <kanyeze@gmail.com>
2024-06-30 07:48:13 +0100

    Create query to get single user name and single job title

--- app/Models/JobHistoryModel.php
+++ app/Models/JobHistoryModel.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\User;
 use Request;
 
 class JobHistoryModel extends Model
@@ -15,10 +16,18 @@ class JobHistoryModel extends Model
     static public function getRecord($request)
     {
         $return = self::select('job_history.*')
-                ->orderBy('id', 'desc')
-                ->paginate(20);
+            ->orderBy('id', 'desc')
+            ->paginate(20);
         return $return;
     }
 
+    public function get_user_name_single()
+    {
+        return $this->belongsTo(User::class, "employee_id");
+    }
 
+    public function get_job_single()
+    {
+        return $this->belongsTo(JobsModel::class,'job_id');
+    }
 }
