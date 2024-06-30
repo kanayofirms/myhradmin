d318bb5053636e274c3cba1dc1e7c0ca5dd553f4
kanayofirms <kanyeze@gmail.com>
2024-06-30 05:08:09 +0100

    Create query for get record function

--- app/Models/JobHistoryModel.php
+++ app/Models/JobHistoryModel.php
@@ -4,12 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Request;
 
 class JobHistoryModel extends Model
 {
     use HasFactory;
 
     protected $table = 'job_history';
+
+    static public function getRecord($request)
+    {
+        $return = self::select('job_history.*')
+                ->orderBy('id', 'desc')
+                ->paginate(20);
+        return $return;
+    }
 
 
 }
