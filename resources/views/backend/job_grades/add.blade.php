e77404bd9978a04b71a0812ef679bdce3cab1e97
kanayofirms <kanyeze@gmail.com>
2024-07-01 22:52:46 +0100

    Refactor code for functionality

--- resources/views/backend/job_grades/add.blade.php
+++ resources/views/backend/job_grades/add.blade.php
@@ -8,12 +8,12 @@
             <div class="container-fluid">
                 <div class="row mb-2">
                     <div class="col-sm-6">
-                        <h1 class="m-0">Job History</h1>
+                        <h1 class="m-0">Job Grades</h1>
                     </div><!-- /.col -->
                     <div class="col-sm-6">
                         <ol class="breadcrumb float-sm-right">
                             <li class="breadcrumb-item"><a href="#">Add</a></li>
-                            <li class="breadcrumb-item active">Job History</li>
+                            <li class="breadcrumb-item active">Job Grades</li>
                         </ol>
                     </div><!-- /.col -->
                 </div><!-- /.row -->
@@ -27,75 +27,46 @@
                     <div class="col-md-12">
                         <div class="card card-info">
                             <div class="card-header">
-                                <h3 class="card-title">Add History</h3>
+                                <h3 class="card-title">Add Job Grades</h3>
                             </div>
 
-                            <form action="{{ url('admin/job_history/add') }}" class="form-horizontal" method="post"
+                            <form action="{{ url('admin/job_grades/add') }}" class="form-horizontal" method="post"
                                 enctype="multipart/form-data">
                                 {{ csrf_field() }}
                                 <div class="card card-body">
+
+
                                     <div class="form-group row">
-                                        <label class="col-sm-2 col-form-label">Employee Name
+                                        <label class="col-sm-2 col-form-label">Grade Level
                                             <span style="color: red;">*</span></label>
                                         <div class="col-sm-10">
-                                            <select name="employee_id" id="" class="form-control">
-                                                <option value="">Select Employee Name</option>
-                                                @foreach ($getEmployee as $value_employee)
-                                                    <option value="{{ $value_employee->id }}">{{ $value_employee->name }}
-                                                        {{ $value_employee->last_name }}</option>
-                                                @endforeach
-                                            </select>
+                                            <input type="text" value="{{ old('grade_level') }}" name="grade_level"
+                                                class="form-control" placeholder="Enter Grade Level" required>
                                         </div>
                                     </div>
 
                                     <div class="form-group row">
-                                        <label class="col-sm-2 col-form-label">Start Date
+                                        <label class="col-sm-2 col-form-label">Lowest Salary
                                             <span style="color: red;">*</span></label>
                                         <div class="col-sm-10">
-                                            <input type="date" value="{{ old('start_date') }}" name="start_date"
-                                                class="form-control" required>
+                                            <input type="number" value="{{ old('lowest_sal') }}" name="lowest_sal"
+                                                class="form-control" placeholder="Enter Lowest Salary" required>
                                         </div>
                                     </div>
 
                                     <div class="form-group row">
-                                        <label class="col-sm-2 col-form-label">End Date
+                                        <label class="col-sm-2 col-form-label">Highest Salary
                                             <span style="color: red;">*</span></label>
                                         <div class="col-sm-10">
-                                            <input type="date" value="{{ old('end_date') }}" name="end_date"
-                                                class="form-control" required>
-                                        </div>
-                                    </div>
-
-                                    <div class="form-group row">
-                                        <label class="col-sm-2 col-form-label">Job Name (Job ID)
-                                            <span style="color: red;">*</span></label>
-                                        <div class="col-sm-10">
-                                            <select name="job_id" id="" class="form-control">
-                                                <option value="">Select Job Name</option>
-                                                @foreach ($getJobs as $value_job)
-                                                    <option value="{{ $value_job->id }}">{{ $value_job->job_title }}
-                                                    </option>
-                                                @endforeach
-                                            </select>
-                                        </div>
-                                    </div>
-
-                                    <div class="form-group row">
-                                        <label class="col-sm-2 col-form-label">Department Name (Department ID)
-                                            <span style="color: red;">*</span></label>
-                                        <div class="col-sm-10">
-                                            <select name="department_id" id="" class="form-control">
-                                                <option value="">Select Department Name</option>
-                                                <option value="1">Customer Care Department</option>
-                                                <option value="2">Sales Department</option>
-                                            </select>
+                                            <input type="number" value="{{ old('highest_sal') }}" name="highest_sal"
+                                                class="form-control" placeholder="Enter Highest Salary" required>
                                         </div>
                                     </div>
 
                                 </div>
 
                                 <div class="card-footer">
-                                    <a href="{{ url('admin/job_history') }}" class="btn btn-default">Back</a>
+                                    <a href="{{ url('admin/job_grades') }}" class="btn btn-default">Back</a>
                                     <button type="submit" class="btn btn-primary float-right">Submit</button>
                                 </div>
                             </form>
