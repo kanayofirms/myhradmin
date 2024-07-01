671c83242bb307d72abbb8252bf78e8b96926ac1
kanayofirms <kanyeze@gmail.com>
2024-07-01 06:35:40 +0100

    Remove closing tag and format for readability

--- app/Http/Controllers/Backend/DashboardController.php
+++ app/Http/Controllers/Backend/DashboardController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Controllers\Backend;
+
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
@@ -12,4 +13,3 @@ class DashboardController extends Controller
     }
 }
 
-?>
