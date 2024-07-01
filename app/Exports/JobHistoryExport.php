eb43754ed558f7ec972c0adb8b01c02b37b36331
kanayofirms <kanyeze@gmail.com>
2024-07-01 14:29:02 +0100

    Refactor code for feasibility and update app

--- app/Exports/JobHistoryExport.php
+++ app/Exports/JobHistoryExport.php
@@ -21,9 +21,22 @@ class JobHistoryExport implements FromCollection, WithMapping, ShouldAutoSize, W
 
     public function map($user): array
     {
+        $startDate = date('Y-m-d', strtotime($user->start_date));
+        $endDate = date('Y-m-d', strtotime($user->end_date));
+        $createdAtFormat = date('d-m-Y H:i A', strtotime($user->created_at));
+        if ($user->department_id == 1) {
+            $department = 'Customer Care Department';
+        } else {
+            $department = 'Sales Department';
+        }
         return [
             $user->id,
-            $user->name . ' ' . $user->last_name
+            $user->name . ' ' . $user->last_name,
+            $startDate,
+            $endDate,
+            $user->job_title,
+            $department,
+            $createdAtFormat
         ];
     }
 
@@ -31,7 +44,12 @@ class JobHistoryExport implements FromCollection, WithMapping, ShouldAutoSize, W
     {
         return [
             'Table ID',
-            'Employee Name'
+            'Employee Name',
+            'Start Date',
+            'End Date',
+            'Job Name',
+            'Department ID',
+            'Created At'
         ];
     }
 }
