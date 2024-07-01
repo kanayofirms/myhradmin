9b1f9ab0c106f977763ed435bc543b9b88e424fd
kanayofirms <kanyeze@gmail.com>
2024-07-01 09:46:29 +0100

    Create excel export button for job history download

--- resources/views/backend/job_history/list.blade.php
+++ resources/views/backend/job_history/list.blade.php
@@ -12,6 +12,16 @@
                     </div><!-- /.col -->
                     <div class="col-sm-6" style="text-align:right;">
 
+                        <form action="{{ url('admin/job_history_export') }}
+
+                        " method="get">
+                            <input type="hidden" name="start_date" value="{{ Request()->start_date }}">
+                            <input type="hidden" name="end_date" value="{{ Request()->end_date }}">
+
+                            <a href="{{ url('admin/job_history_export?start_date=' . Request::get('start_date') . '&end_date=' . Request::get('end_date')) }}"
+                                class="btn btn-success">Excel Export</a>
+                        </form>
+                        <br>
                         <a href="{{ url('admin/job_history/add') }}" class="btn btn-primary">
                             Add Job History
                         </a>
