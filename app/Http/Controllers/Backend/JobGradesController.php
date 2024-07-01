8e5b5a9cb7041e696f9d0e41a3ed96c79cce0406
kanayofirms <kanyeze@gmail.com>
2024-07-01 20:56:41 +0100

    Create add function to add job grades

--- app/Http/Controllers/Backend/JobGradesController.php
+++ app/Http/Controllers/Backend/JobGradesController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Backend;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Models\JobGradesModel;
 
 
 class JobGradesController extends Controller
@@ -11,5 +12,10 @@ class JobGradesController extends Controller
     public function index()
     {
         return view('backend.job_grades.list');
+    }
+
+    public function add(Request $request)
+    {
+        return view('backend.job_grades.add');
     }
 }
