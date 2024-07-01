d77762ca26f24750453ca90bbbf7909f884d9882
kanayofirms <kanyeze@gmail.com>
2024-07-01 17:25:40 +0100

    Refactor view for feasibility

--- resources/views/backend/job_grades/list.blade.php
+++ resources/views/backend/job_grades/list.blade.php
@@ -1,1 +1,56 @@
-Hello World!
+@extends('backend.layouts.app')
+
+@section('content')
+    <!-- Content Wrapper. Contains page content -->
+    <div class="content-wrapper">
+        <!-- Content Header (Page header) -->
+        <div class="content-header">
+            <div class="container-fluid">
+                <div class="row mb-2">
+                    <div class="col-sm-6">
+                        <h1>Job Grades</h1>
+                    </div><!-- /.col -->
+                    <div class="col-sm-6" style="text-align:right;">
+
+                        <a href="{{ url('admin/job_grades/add') }}" class="btn btn-primary">
+                            Add Job Grades
+                        </a>
+                    </div><!-- /.col -->
+                </div><!-- /.row -->
+            </div><!-- /.container-fluid -->
+        </div>
+        <!-- /.content-header -->
+
+        <section class="content">
+            <div class="container-fluid">
+                <div class="row">
+                    <section class="col-md-12">
+
+                        @include('_message')
+
+                        <div class="card">
+                            <div class="card-header">
+                                <h3 class="card-title">Job Grades List</h3>
+                            </div>
+                            <div class="card-body p-0">
+                                <table class="table table-striped">
+                                    <thead>
+
+                                    </thead>
+                                    <tbody>
+
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </section>
+                </div>
+            </div>
+
+        </section>
+
+
+        <!-- /.content -->
+    </div>
+    <!-- /.content-wrapper -->
+@endsection
