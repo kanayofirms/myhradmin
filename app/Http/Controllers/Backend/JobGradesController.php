3fc4649292cb0a39d6fb280635054f212ace4f21
kanayofirms <kanyeze@gmail.com>
2024-07-01 23:13:25 +0100

    Create add post function with validation to insert job grades into db

--- app/Http/Controllers/Backend/JobGradesController.php
+++ app/Http/Controllers/Backend/JobGradesController.php
@@ -18,4 +18,23 @@ class JobGradesController extends Controller
     {
         return view('backend.job_grades.add');
     }
+
+    public function add_post(Request $request)
+    {
+        // dd($request->all());
+        $user = request()->validate([
+            'grade_level' => 'required',
+            'lowest_sal' => 'required',
+            'highest_sal' => 'required'
+        ]);
+
+
+        $user = new JobGradesModel;
+        $user->grade_level = trim($request->grade_level);
+        $user->lowest_sal = trim($request->lowest_sal);
+        $user->highest_sal = trim($request->highest_sal);
+        $user->save();
+
+        return redirect('admin/job_grades')->with('success', 'Job grades successfully added.');
+    }
 }
