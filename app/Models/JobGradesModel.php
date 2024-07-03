b3ec3c10112dcbcac333e64620238fc95815f0e4
kanayofirms <kanyeze@gmail.com>
2024-07-03 03:41:27 +0100

    Set input values in search form

--- app/Models/JobGradesModel.php
+++ app/Models/JobGradesModel.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Request;
 
 class JobGradesModel extends Model
 {
@@ -13,10 +14,44 @@ class JobGradesModel extends Model
 
     static public function getRecord($request)
     {
-        $return = self::select('job_grades.*')
-            ->orderBy('id', 'desc')
-            ->paginate(20);
+        // $return = self::select('job_grades.*')
+        //     ->orderBy('id', 'desc')
+        //     ->paginate(20);
 
+        // return $return;
+
+        $return = self::select('job_grades.*');
+
+        // search box start
+        if (!empty(Request::get('id'))) {
+            $return = $return->where('id', '=', Request::get('id'));
+        }
+
+        if (!empty(Request::get('grade_level'))) {
+            $return = $return->where('grade_level', 'like', '%' . Request::get('grade_level') . '%');
+        }
+
+        if (!empty(Request::get('lowest_sal'))) {
+            $return = $return->where('lowest_sal', 'like', '%' . Request::get('lowest_sal') . '%');
+        }
+
+        if (!empty(Request::get('highest_sal'))) {
+            $return = $return->where('highest_sal', 'like', '%' . Request::get('highest_sal') . '%');
+        }
+
+        if (!empty(Request::get('created_at'))) {
+            $return = $return->where('created_at', 'like', '%' . Request::get('created_at') . '%');
+        }
+
+        if (!empty(Request::get('updated_at'))) {
+            $return = $return->where('updated_at', 'like', '%' . Request::get('updated_at') . '%');
+        }
+
+        // search box end
+
+        $return = $return->orderBy('id', 'desc')
+            ->paginate(20);
         return $return;
+
     }
 }
