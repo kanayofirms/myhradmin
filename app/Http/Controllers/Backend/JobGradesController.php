81bee67a76d59541f8a9deb142c6269311017654
kanayofirms <kanyeze@gmail.com>
2024-07-03 09:30:17 +0100

    Create function to insert updated job grades record into the db

--- app/Http/Controllers/Backend/JobGradesController.php
+++ app/Http/Controllers/Backend/JobGradesController.php
@@ -44,4 +44,16 @@ class JobGradesController extends Controller
         $data['getRecord'] = JobGradesModel::find($id);
         return view('backend.job_grades.edit', $data);
     }
+
+    public function edit_update(Request $request, $id)
+    {
+        $user = JobGradesModel::find($id);
+
+        $user->grade_level = trim($request->grade_level);
+        $user->lowest_sal = trim($request->lowest_sal);
+        $user->highest_sal = trim($request->highest_sal);
+        $user->save();
+
+        return redirect('admin/job_grades')->with('success', 'Job Grades successfully updated.');
+    }
 }
