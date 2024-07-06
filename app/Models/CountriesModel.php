e181237880770bfc0b508288671fd60fe5494c0c
kanayofirms <kanyeze@gmail.com>
2024-07-06 04:52:25 +0100

    Refactor model for functionality

--- app/Models/CountriesModel.php
+++ app/Models/CountriesModel.php
@@ -4,8 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Request;
 
 class CountriesModel extends Model
 {
     use HasFactory;
+
+    protected $table = 'countries';
 }
