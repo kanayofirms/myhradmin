ee2e1835760453603877206a27589d36cf021cc3
kanayofirms <kanyeze@gmail.com>
2024-07-06 04:51:22 +0100

    Create view for index

---
+++ resources/views/backend/countries/list.blade.php
@@ -0,0 +1,39 @@
+@extends('backend.layouts.app')
+
+@section('content')
+    <!-- Content Wrapper. Contains page content -->
+    <div class="content-wrapper">
+        <!-- Content Header (Page header) -->
+        <div class="content-header">
+            <div class="container-fluid">
+                <div class="row mb-2">
+                    <div class="col-sm-6">
+                        <h1>Countries</h1>
+                    </div><!-- /.col -->
+                    <div class="col-sm-6" style="text-align:right;">
+
+                        <a href="{{ url('admin/countries/add') }}" class="btn btn-primary">
+                            Add Countries
+                        </a>
+                    </div><!-- /.col -->
+                </div><!-- /.row -->
+            </div><!-- /.container-fluid -->
+        </div>
+        <!-- /.content-header -->
+
+        <section class="content">
+            <div class="container-fluid">
+
+                <div class="row">
+                    <section class="col-md-12">
+
+                    </section>
+
+                </div>
+            </div>
+        </section>
+
+        <!-- /.content -->
+    </div>
+    <!-- /.content-wrapper -->
+@endsection
