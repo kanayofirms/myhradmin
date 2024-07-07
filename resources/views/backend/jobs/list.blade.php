1fec454d0ad6d2ce2faaf41c5f11b7fb77ef1370
kanayofirms <kanyeze@gmail.com>
2024-07-07 06:07:21 +0100

    Create pagination

--- resources/views/backend/jobs/list.blade.php
+++ resources/views/backend/jobs/list.blade.php
@@ -141,7 +141,7 @@
                                     </tbody>
                                 </table>
                                 <div style="padding:10px; float:right;">
-
+                                    {!! $getRecord->appends(Illuminate\Support\Facades\Request::except('page'))->links() !!}
                                 </div>
                             </div>
 
