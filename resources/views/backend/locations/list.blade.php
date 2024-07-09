e86df622941b77f03e127a8868c62185ec9b7ba6
kanayofirms <kanyeze@gmail.com>
2024-07-09 15:57:26 +0100

    Crete pagination

--- resources/views/backend/locations/list.blade.php
+++ resources/views/backend/locations/list.blade.php
@@ -69,6 +69,7 @@
                                     </tbody>
                                 </table>
                                 <div style="padding:10px; float:right;">
+                                    {!! $getRecord->appends(Illuminate\Support\Facades\Request::except('page'))->links() !!}
 
                                 </div>
                             </div>
