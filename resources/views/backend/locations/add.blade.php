d23e4fa3de5a85a792d0283d6f92c23866583cb8
kanayofirms <kanyeze@gmail.com>
2024-07-09 12:21:42 +0100

    Create foreach loop to display countries values in a dropdowm dynamically

--- resources/views/backend/locations/add.blade.php
+++ resources/views/backend/locations/add.blade.php
@@ -8,12 +8,12 @@
             <div class="container-fluid">
                 <div class="row mb-2">
                     <div class="col-sm-6">
-                        <h1 class="m-0">Job History</h1>
+                        <h1 class="m-0">Locations</h1>
                     </div><!-- /.col -->
                     <div class="col-sm-6">
                         <ol class="breadcrumb float-sm-right">
                             <li class="breadcrumb-item"><a href="#">Add</a></li>
-                            <li class="breadcrumb-item active">Job History</li>
+                            <li class="breadcrumb-item active">Locations</li>
                         </ol>
                     </div><!-- /.col -->
                 </div><!-- /.row -->
@@ -27,61 +27,62 @@
                     <div class="col-md-12">
                         <div class="card card-info">
                             <div class="card-header">
-                                <h3 class="card-title">Add Job History</h3>
+                                <h3 class="card-title">Add Locations</h3>
                             </div>
 
-                            <form action="{{ url('admin/job_history/add') }}" class="form-horizontal" method="post"
+                            <form action="{{ url('admin/locations/add') }}" class="form-horizontal" method="post"
                                 enctype="multipart/form-data">
                                 {{ csrf_field() }}
                                 <div class="card card-body">
+
                                     <div class="form-group row">
-                                        <label class="col-sm-2 col-form-label">Employee Name
+                                        <label class="col-sm-2 col-form-label">Street Address
                                             <span style="color: red;">*</span></label>
                                         <div class="col-sm-10">
-                                            <select name="employee_id" id="" class="form-control">
-                                                <option value="">Select Employee Name</option>
-
-                                            </select>
+                                            <input type="text" value="{{ old('street_address') }}" name="street_address"
+                                                class="form-control" placeholder="Enter Street Address" required>
                                         </div>
                                     </div>
 
                                     <div class="form-group row">
-                                        <label class="col-sm-2 col-form-label">Start Date
+                                        <label class="col-sm-2 col-form-label">Postal Code
                                             <span style="color: red;">*</span></label>
                                         <div class="col-sm-10">
-                                            <input type="date" value="{{ old('start_date') }}" name="start_date"
-                                                class="form-control" required>
+                                            <input type="text" value="{{ old('postal_code') }}" name="postal_code"
+                                                class="form-control" placeholder="Enter Postal Code" required>
                                         </div>
                                     </div>
 
                                     <div class="form-group row">
-                                        <label class="col-sm-2 col-form-label">End Date
+                                        <label class="col-sm-2 col-form-label">City
                                             <span style="color: red;">*</span></label>
                                         <div class="col-sm-10">
-                                            <input type="date" value="{{ old('end_date') }}" name="end_date"
-                                                class="form-control" required>
+                                            <input type="text" value="{{ old('city') }}" name="city"
+                                                class="form-control" placeholder="Enter City" required>
                                         </div>
                                     </div>
 
                                     <div class="form-group row">
-                                        <label class="col-sm-2 col-form-label">Job Name (Job ID)
+                                        <label class="col-sm-2 col-form-label">State/Province
                                             <span style="color: red;">*</span></label>
                                         <div class="col-sm-10">
-                                            <select name="job_id" id="" class="form-control">
-                                                <option value="">Select Job Name</option>
-
-                                            </select>
+                                            <input type="text" value="{{ old('state_province') }}" name="state_province"
+                                                class="form-control" placeholder="Enter State/Province" required>
                                         </div>
                                     </div>
 
+
                                     <div class="form-group row">
-                                        <label class="col-sm-2 col-form-label">Department Name (Department ID)
+                                        <label class="col-sm-2 col-form-label">Country (Countries ID)
                                             <span style="color: red;">*</span></label>
                                         <div class="col-sm-10">
-                                            <select name="department_id" id="" class="form-control">
-                                                <option value="">Select Department Name</option>
-                                                <option value="1">Customer Care Department</option>
-                                                <option value="2">Sales Department</option>
+                                            <select name="department_id" id="" class="form-control" required>
+                                                <option value="">Select Country</option>
+                                                @foreach ($getCountries as $countries)
+                                                    <option value="{{ $countries->id }}">{{ $countries->country_name }}
+                                                    </option>
+                                                @endforeach
+
                                             </select>
                                         </div>
                                     </div>
@@ -89,7 +90,7 @@
                                 </div>
 
                                 <div class="card-footer">
-                                    <a href="{{ url('admin/job_history') }}" class="btn btn-default">Back</a>
+                                    <a href="{{ url('admin/locations') }}" class="btn btn-default">Back</a>
                                     <button type="submit" class="btn btn-primary float-right">Submit</button>
                                 </div>
                             </form>
