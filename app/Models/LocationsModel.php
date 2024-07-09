6117addd9979d5cad86d621d7ca4ee0acd2a82f7
kanayofirms <kanyeze@gmail.com>
2024-07-09 15:04:55 +0100

    Create function to get locations record with orderBy query

--- app/Models/LocationsModel.php
+++ app/Models/LocationsModel.php
@@ -11,4 +11,13 @@ class LocationsModel extends Model
     use HasFactory;
 
     protected $table = 'locations';
+
+    static public function getRecord($request)
+    {
+        $return = self::select('locations.*')
+            ->orderBy('id', 'desc')
+            ->paginate(20);
+
+        return $return;
+    }
 }
