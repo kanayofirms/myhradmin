95dc69d89a5664190dfe071bcfb25498d1e9d9d8
kanayofirms <kanyeze@gmail.com>
2024-07-11 05:54:46 +0100

    Create like queries for search in getRecord function

--- app/Models/LocationsModel.php
+++ app/Models/LocationsModel.php
@@ -18,6 +18,39 @@ class LocationsModel extends Model
             ->join('countries', 'countries.id', '=', 'locations.countries_id')
             ->orderBy('id', 'desc');
 
+        //Search start
+        if (!empty(Request::get('id'))) {
+            $return = $return->where('locations.id', '=', Request::get('id'));
+        }
+
+        if (!empty(Request::get('street_address'))) {
+            $return = $return->where('locations.street_address', 'like', '%' . Request::get('street_address') . '%');
+        }
+
+        if (!empty(Request::get('postal_code'))) {
+            $return = $return->where('locations.postal_code', 'like', '%' . Request::get('postal_code') . '%');
+        }
+
+        if (!empty(Request::get('city'))) {
+            $return = $return->where('locations.city', 'like', '%' . Request::get('city') . '%');
+        }
+
+        if (!empty(Request::get('state_province'))) {
+            $return = $return->where('locations.state_province', 'like', '%' . Request::get('state_province') . '%');
+        }
+
+        if (!empty(Request::get('country_name'))) {
+            $return = $return->where('countries.country_name', 'like', '%' . Request::get('country_name') . '%');
+        }
+
+        if (!empty(Request::get('created_at'))) {
+            $return = $return->where('locations.created_at', 'like', '%' . Request::get('created_at') . '%');
+        }
+
+        if (!empty(Request::get('updated_at'))) {
+            $return = $return->where('locations.updated_at', 'like', '%' . Request::get('updated_at') . '%');
+        }
+
         $return = $return->paginate(20);
 
         return $return;
