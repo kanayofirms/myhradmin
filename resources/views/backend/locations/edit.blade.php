013ead5df8f6d33c22203c501389b2d25b20bcd3
kanayofirms <kanyeze@gmail.com>
2024-07-11 11:16:15 +0100

    Create edit on view

---
+++ resources/views/backend/locations/edit.blade.php
@@ -0,0 +1,105 @@
+@extends('backend.layouts.app')
+
+@section('content')
+    <!-- Content Wrapper. Contains page content -->
+    <div class="content-wrapper">
+        <!-- Content Header (Page header) -->
+        <div class="content-header">
+            <div class="container-fluid">
+                <div class="row mb-2">
+                    <div class="col-sm-6">
+                        <h1 class="m-0">Locations</h1>
+                    </div><!-- /.col -->
+                    <div class="col-sm-6">
+                        <ol class="breadcrumb float-sm-right">
+                            <li class="breadcrumb-item"><a href="#">Edit</a></li>
+                            <li class="breadcrumb-item active">Locations</li>
+                        </ol>
+                    </div><!-- /.col -->
+                </div><!-- /.row -->
+            </div><!-- /.container-fluid -->
+        </div>
+        <!-- /.content-header -->
+
+        <section class="content">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="card card-info">
+                            <div class="card-header">
+                                <h3 class="card-title">Edit Locations</h3>
+                            </div>
+
+                            <form action="{{ url('admin/locations/add' . $getRecord->id) }}" class="form-horizontal"
+                                method="post" enctype="multipart/form-data">
+                                {{ csrf_field() }}
+                                <div class="card card-body">
+
+                                    <div class="form-group row">
+                                        <label class="col-sm-2 col-form-label">Street Address
+                                            <span style="color: red;">*</span></label>
+                                        <div class="col-sm-10">
+                                            <input type="text" value="{{ $getRecord->street_address }}"
+                                                name="street_address" class="form-control"
+                                                placeholder="Enter Street Address" required>
+                                        </div>
+                                    </div>
+
+                                    <div class="form-group row">
+                                        <label class="col-sm-2 col-form-label">Postal Code
+                                            <span style="color: red;">*</span></label>
+                                        <div class="col-sm-10">
+                                            <input type="number" value="{{ $getRecord->postal_code }}" name="postal_code"
+                                                class="form-control" placeholder="Enter Postal Code" required>
+                                        </div>
+                                    </div>
+
+                                    <div class="form-group row">
+                                        <label class="col-sm-2 col-form-label">City
+                                            <span style="color: red;">*</span></label>
+                                        <div class="col-sm-10">
+                                            <input type="text" value="{{ $getRecord->city }}" name="city"
+                                                class="form-control" placeholder="Enter City" required>
+                                        </div>
+                                    </div>
+
+                                    <div class="form-group row">
+                                        <label class="col-sm-2 col-form-label">State/Province
+                                            <span style="color: red;">*</span></label>
+                                        <div class="col-sm-10">
+                                            <input type="text" value="{{ $getRecord->state_province }}"
+                                                name="state_province" class="form-control"
+                                                placeholder="Enter State/Province" required>
+                                        </div>
+                                    </div>
+
+
+                                    <div class="form-group row">
+                                        <label class="col-sm-2 col-form-label">Country (Countries ID)
+                                            <span style="color: red;">*</span></label>
+                                        <div class="col-sm-10">
+                                            <select name="countries_id" id="" class="form-control" required>
+                                                <option value="">Select Country</option>
+
+
+                                            </select>
+                                        </div>
+                                    </div>
+
+                                </div>
+
+                                <div class="card-footer">
+                                    <a href="{{ url('admin/locations') }}" class="btn btn-default">Back</a>
+                                    <button type="submit" class="btn btn-primary float-right">Update</button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <!-- /.content -->
+    </div>
+    <!-- /.content-wrapper -->
+@endsection
