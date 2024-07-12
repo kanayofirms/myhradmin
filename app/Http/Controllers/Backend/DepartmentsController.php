e7b2363cf4911b61c3d38542c3d28d534a0dedf4
kanayofirms <kanyeze@gmail.com>
2024-07-12 10:24:15 +0100

    Create add post function to insert departments record into the db

--- app/Http/Controllers/Backend/DepartmentsController.php
+++ app/Http/Controllers/Backend/DepartmentsController.php
@@ -19,4 +19,22 @@ class DepartmentsController extends Controller
         $data['getLocation'] = LocationsModel::get();
         return view('backend.departments.add', $data);
     }
+
+    public function add_post(Request $request)
+    {
+        // dd($request->all());
+        $user = request()->validate([
+            'department_name' => 'required',
+            'manager_id' => 'required',
+            'locations_id' => 'required'
+        ]);
+
+        $user = new DepartmentsModel;
+        $user->department_name = trim($request->department_name);
+        $user->manager_id = trim($request->manager_id);
+        $user->locations_id = trim($request->locations_id);
+        $user->save();
+
+        return redirect('admin/departments')->with('success', 'Departments successfully added.');
+    }
 }
