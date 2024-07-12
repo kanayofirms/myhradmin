2f7d000664ac5c499a9caeba4c9afb9b12d28213
kanayofirms <kanyeze@gmail.com>
2024-07-12 04:02:50 +0100

    Create model for departments

---
+++ app/Models/DepartmentsModel.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Request;
+
+class DepartmentsModel extends Model
+{
+    use HasFactory;
+
+    protected $table = "departments";
+
+}
