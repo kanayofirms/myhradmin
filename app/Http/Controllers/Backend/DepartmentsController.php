c2a927dd1c9d4bd35fbbb1b97531e3734b0f5f3b
kanayofirms <kanyeze@gmail.com>
2024-07-12 09:13:26 +0100

    Fetch locations in add function and pass data to view

--- app/Http/Controllers/Backend/DepartmentsController.php
+++ app/Http/Controllers/Backend/DepartmentsController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Backend;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Models\DepartmentsModel;
+use App\Models\LocationsModel;
 
 class DepartmentsController extends Controller
 {
@@ -15,6 +16,7 @@ class DepartmentsController extends Controller
 
     public function add(Request $request)
     {
-        return view('backend.departments.add');
+        $data['getLocation'] = LocationsModel::get();
+        return view('backend.departments.add', $data);
     }
 }
