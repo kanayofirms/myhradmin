34673766273752ee66109452a594fdc028c548ab
kanayofirms <kanyeze@gmail.com>
2024-07-12 04:02:26 +0100

    Create index function for departments

---
+++ app/Http/Controllers/Backend/DepartmentsController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\Backend;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Models\DepartmentsModel;
+
+class DepartmentsController extends Controller
+{
+    public function index(Request $request)
+    {
+        return view("backend.departments.list");
+    }
+}
