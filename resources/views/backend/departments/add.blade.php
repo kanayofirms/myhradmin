a84702c1562921032f90742965d9fd2d71173350
kanayofirms <kanyeze@gmail.com>
2024-07-12 04:47:00 +0100

    Create view for add departments

---
+++ resources/views/backend/departments/add.blade.php
@@ -0,0 +1,88 @@
+@extends('backend.layouts.app')
+
+@section('content')
+    <!-- Content Wrapper. Contains page content -->
+    <div class="content-wrapper">
+        <!-- Content Header (Page header) -->
+        <div class="content-header">
+            <div class="container-fluid">
+                <div class="row mb-2">
+                    <div class="col-sm-6">
+                        <h1 class="m-0">Departments</h1>
+                    </div><!-- /.col -->
+                    <div class="col-sm-6">
+                        <ol class="breadcrumb float-sm-right">
+                            <li class="breadcrumb-item"><a href="#">Add</a></li>
+                            <li class="breadcrumb-item active">Departments</li>
+                        </ol>
+                    </div><!-- /.col -->
+                </div><!-- /.row -->
+            </div><!-- /.container-fluid -->
+        </div>
+        <!-- /.content-header -->
+
+        <section class="content">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="card card-info">
+                            <div class="card-header">
+                                <h3 class="card-title">Add Departments</h3>
+                            </div>
+
+                            <form action="{{ url('admin/departments/add') }}" class="form-horizontal" method="post"
+                                enctype="multipart/form-data">
+                                {{ csrf_field() }}
+                                <div class="card card-body">
+
+                                    <div class="form-group row">
+                                        <label class="col-sm-2 col-form-label">Department Name
+                                            <span style="color: red;">*</span></label>
+                                        <div class="col-sm-10">
+                                            <input type="text" value="{{ old('department_name') }}"
+                                                name="department_name" class="form-control"
+                                                placeholder="Enter Department Name" required>
+                                        </div>
+                                    </div>
+
+
+                                    <div class="form-group row">
+                                        <label class="col-sm-2 col-form-label">Manager Name (Manager ID)
+                                            <span style="color: red;">*</span></label>
+                                        <div class="col-sm-10">
+                                            <select name="manager_id" class="form-control" required>
+                                                <option value="">Select Manager</option>
+                                                <option value="1">ChiChi</option>
+                                                <option value="2">Emeka</option>
+                                            </select>
+                                        </div>
+                                    </div>
+
+                                    <div class="form-group row">
+                                        <label class="col-sm-2 col-form-label">Locations Name (Locations ID)
+                                            <span style="color: red;">*</span></label>
+                                        <div class="col-sm-10">
+                                            <select name="locations_id" class="form-control" required>
+                                                <option value="">Select Location</option>
+
+                                            </select>
+                                        </div>
+                                    </div>
+
+                                </div>
+
+                                <div class="card-footer">
+                                    <a href="{{ url('admin/departments') }}" class="btn btn-default">Back</a>
+                                    <button type="submit" class="btn btn-primary float-right">Submit</button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <!-- /.content -->
+    </div>
+    <!-- /.content-wrapper -->
+@endsection
