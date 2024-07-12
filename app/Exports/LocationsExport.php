c7cd899ab1589c0c2acf05bb69e9fc7ce2eb4fc1
kanayofirms <kanyeze@gmail.com>
2024-07-12 03:30:40 +0100

    Create export file for locations record

---
+++ app/Exports/LocationsExport.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Exports;
+
+use App\Models\LocationsModel;
+use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\WithHeadings;
+use Maatwebsite\Excel\Concerns\WithMapping;
+use Maatwebsite\Excel\Concerns\ShouldAutoSize;
+use Request;
+
+
+class LocationsExport implements FromCollection, WithMapping, ShouldAutoSize, WithHeadings
+{
+    public function collection()
+    {
+        $request = Request::all();
+        return LocationsModel::getRecord($request);
+    }
+
+    protected $index = 0;
+
+    public function map($user): array
+    {
+        $createdAtFormat = date('d-m-Y', strtotime($user->created_at));
+        return [
+            ++$this->index,
+            $user->id,
+            $user->street_address,
+            $user->postal_code,
+            $user->city,
+            $user->state_province,
+            $user->country_name,
+            $createdAtFormat
+        ];
+    }
+
+    public function headings(): array
+    {
+        return [
+            'S.No',
+            'Table ID',
+            'Street Address',
+            'Postal Code',
+            'City',
+            'State/Province',
+            'Country',
+            'Created At',
+        ];
+    }
+
+}
+
