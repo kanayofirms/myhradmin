2ddd290ce44f871db3a3bf699d233efdbdfcf6f5
kanayofirms <kanyeze@gmail.com>
2024-07-14 21:27:52 +0100

    Create GET route to edit departments record by id

--- routes/web.php
+++ routes/web.php
@@ -123,6 +123,7 @@ Route::group(['middleware' => 'admin'], function () {
     Route::get('admin/departments', [DepartmentsController::class, 'index']);
     Route::get('admin/departments/add', [DepartmentsController::class, 'add']);
     Route::post('admin/departments/add', [DepartmentsController::class, 'add_post']);
+    Route::get('admin/departments/edit/{id}', [DepartmentsController::class, 'edit']);
 });
 
 Route::get('logout', [AuthController::class, 'logout']);
