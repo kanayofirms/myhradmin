35c180263736ab0a546c115b0417b2c83c00ad79
kanayofirms <kanyeze@gmail.com>
2024-07-16 12:00:49 +0100

    Refactor code to get manager record for data persitence

---
+++ resources/views/backend/manager/edit.blade.php
@@ -0,0 +1,81 @@
+@extends('backend.layouts.app')
+
+@section('content')
+    <!-- Content Wrapper. Contains page content -->
+    <div class="content-wrapper">
+        <!-- Content Header (Page header) -->
+        <div class="content-header">
+            <div class="container-fluid">
+                <div class="row mb-2">
+                    <div class="col-sm-6">
+                        <h1 class="m-0">Manager</h1>
+                    </div><!-- /.col -->
+                    <div class="col-sm-6">
+                        <ol class="breadcrumb float-sm-right">
+                            <li class="breadcrumb-item"><a href="#">Edit</a></li>
+                            <li class="breadcrumb-item active">Manager</li>
+                        </ol>
+                    </div><!-- /.col -->
+                </div><!-- /.row -->
+            </div><!-- /.container-fluid -->
+        </div>
+        <!-- /.content-header -->
+
+        <section class="content">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="card card-info">
+                            <div class="card-header">
+                                <h3 class="card-title">Edit Manager</h3>
+                            </div>
+
+                            <form action="{{ url('admin/manager/edit/' . $getRecord->id) }}" class="form-horizontal"
+                                method="post" enctype="multipart/form-data">
+                                {{ csrf_field() }}
+                                <div class="card card-body">
+                                    <div class="form-group row">
+                                        <label class="col-sm-2 col-form-label">Manager Name
+                                            <span style="color: red;">*</span></label>
+                                        <div class="col-sm-10">
+                                            <input type="text" value="{{ $getRecord->manager_name }}" name="manager_name"
+                                                class="form-control" required placeholder="John Doe">
+                                            <span style="color:red;">{{ $errors->first('manager_name') }}</span>
+                                        </div>
+                                    </div>
+
+                                    <div class="form-group row">
+                                        <label class="col-sm-2 col-form-label">Manager Email
+                                            <span style="color: red;">*</span></label>
+                                        <div class="col-sm-10">
+                                            <input type="email" value="{{ $getRecord->manager_email }}"
+                                                name="manager_email" class="form-control" required
+                                                placeholder="managerceo@mailnator.com">
+                                        </div>
+                                    </div>
+
+                                    <div class="form-group row">
+                                        <label class="col-sm-2 col-form-label">Manager Phone
+                                            <span style="color: red;">*</span></label>
+                                        <div class="col-sm-10">
+                                            <input type="number" value="{{ $getRecord->manager_phone }}"
+                                                name="manager_phone" class="form-control" required
+                                                placeholder="08083093091">
+                                        </div>
+                                    </div>
+
+                                    <div class="card-footer">
+                                        <a href="{{ url('admin/manager') }}" class="btn btn-default">Back</a>
+                                        <button type="submit" class="btn btn-primary float-right">Update</button>
+                                    </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <!-- /.content -->
+    </div>
+    <!-- /.content-wrapper -->
+@endsection
