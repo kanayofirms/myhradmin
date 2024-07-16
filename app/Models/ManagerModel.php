77c07169df170fffc6eb877daeeef0c72c41f4a4
kanayofirms <kanyeze@gmail.com>
2024-07-16 13:07:30 +0100

    Refactor model with search query for scalability

--- app/Models/ManagerModel.php
+++ app/Models/ManagerModel.php
@@ -14,10 +14,39 @@ class ManagerModel extends Model
 
     static public function getRecord($request)
     {
-        $return = self::select('manager.*')
-            ->orderBy('id', 'desc')
-            ->paginate(20);
+        // $return = self::select('manager.*')
+        //     ->orderBy('id', 'desc')
+        //     ->paginate(20);
 
+        // return $return;
+
+        $return = self::select('manager.*');
+
+        //search box start
+        if (!empty(Request::get('id'))) {
+            $return = $return->where('id', '=', Request::get('id'));
+        }
+
+        if (!empty(Request::get('manager_name'))) {
+            $return = $return->where('manager_name', 'LIKE', '%' . Request::get('manager_name') . '%');
+        }
+
+        if (!empty(Request::get('manager_email'))) {
+            $return = $return->where('manager_email', 'LIKE', '%' . Request::get('manager_email') . '%');
+        }
+
+        if (!empty(Request::get('manager_phone'))) {
+            $return = $return->where('manager_phone', 'LIKE', '%' . Request::get('manager_phone') . '%');
+        }
+
+        if (!empty(Request::get('start_date')) && !empty(Request::get('end_date'))) {
+            $return = $return->where('jobs.created_at', '>=', Request::get('start_date'))
+                ->where('jobs.created_at', '<=', Request::get('end_date'));
+        }
+        //search box end
+
+        $return = $return->orderBy('id', 'desc')
+            ->paginate(20);
         return $return;
     }
 }
