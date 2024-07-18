d8036e1c2cbac53bf6ebd7aa4bbe15f615e1dfa8
kanayofirms <kanyeze@gmail.com>
2024-07-18 18:22:38 +0100

    Refactor code for dynamic fetching department name record for export

--- app/Exports/JobHistoryExport.php
+++ app/Exports/JobHistoryExport.php
@@ -24,18 +24,18 @@ class JobHistoryExport implements FromCollection, WithMapping, ShouldAutoSize, W
         $startDate = date('Y-m-d', strtotime($user->start_date));
         $endDate = date('Y-m-d', strtotime($user->end_date));
         $createdAtFormat = date('d-m-Y H:i A', strtotime($user->created_at));
-        if ($user->department_id == 1) {
-            $department = 'Customer Care Department';
-        } else {
-            $department = 'Sales Department';
-        }
+        // if ($user->department_id == 1) {
+        //     $department = 'Customer Care Department';
+        // } else {
+        //     $department = 'Sales Department';
+        // }
         return [
             $user->id,
             $user->name . ' ' . $user->last_name,
             $startDate,
             $endDate,
             $user->job_title,
-            $department,
+            $user->department_name,
             $createdAtFormat
         ];
     }
