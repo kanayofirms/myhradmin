f44983c26fbd68ce4ef957079793b2077049c612
kanayofirms <kanyeze@gmail.com>
2024-07-18 10:15:51 +0100

    Create function to validate and insert updated my account record

--- app/Http/Controllers/Backend/MyAccountController.php
+++ app/Http/Controllers/Backend/MyAccountController.php
@@ -14,4 +14,23 @@ class MyAccountController extends Controller
         $data['getRecord'] = User::find(Auth::user()->id);
         return view("backend.my_account.update", $data);
     }
+
+    public function edit_update(Request $request)
+    {
+        // dd($request->all());
+
+        $user = request()->validate([
+            'email' => 'required|unique:users,email,' . Auth::user()->id
+        ]);
+
+        $user = User::find(Auth::user()->id);
+        $user->name = trim($request->name);
+        $user->email = trim($request->email);
+        if (!empty($request->password)) {
+            $user->password = trim($request->password);
+        }
+        $user->save();
+
+        return redirect('admin/my_account')->with('success', 'My Account Successfully Upated!');
+    }
 }
