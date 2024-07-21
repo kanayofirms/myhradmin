ee04bd349ba81355210005361e34e60f02f80ad9
kanayofirms <kanyeze@gmail.com>
2024-07-21 03:56:06 +0100

    Update profile image route for dynamic upload

--- resources/views/backend/layouts/_sidebar.blade.php
+++ resources/views/backend/layouts/_sidebar.blade.php
@@ -42,8 +42,11 @@
          <!-- Sidebar user panel (optional) -->
          <div class="user-panel mt-3 pb-3 mb-3 d-flex">
              <div class="image">
-                 <img src="{{ url('public/backend/dist/img/user2-160x160.jpg') }}" class="img-circle elevation-2"
-                     alt="User Image">
+                 @if (Auth::user()->profile_image)
+                     <img src="{{ url('upload/' . Auth::user()->profile_image) }}" class="img-circle elevation-2"
+                         alt="User Image">
+                 @endif
+
              </div>
              <div class="info">
                  <a href="" class="d-block">{{ Auth::user()->name }}</a>
