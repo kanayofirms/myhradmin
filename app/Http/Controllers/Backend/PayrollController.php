aebd8de7da46fa3f7aa3499ed1d8db6e4e1e942b
kanayofirms <kanyeze@gmail.com>
2024-07-23 11:46:31 +0100

    Create add function to add payroll

--- app/Http/Controllers/Backend/PayrollController.php
+++ app/Http/Controllers/Backend/PayrollController.php
@@ -4,11 +4,17 @@ namespace App\Http\Controllers\Backend;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\ModelsPayrollModel;
 
 class PayrollController extends Controller
 {
     public function index(Request $request)
     {
         return view('backend.payroll.list');
+    }
+
+    public function add(Request $request)
+    {
+        return view('backend.payroll.add');
     }
 }
