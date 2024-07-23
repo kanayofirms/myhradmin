b25036c0184be2f62dca9366256830e0491cf709
kanayofirms <kanyeze@gmail.com>
2024-07-23 15:22:11 +0100

    Refactor add function to fetch employee by role and pass data to view

--- app/Http/Controllers/Backend/PayrollController.php
+++ app/Http/Controllers/Backend/PayrollController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Backend;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\ModelsPayrollModel;
+use App\Models\User;
 
 class PayrollController extends Controller
 {
@@ -15,6 +16,7 @@ class PayrollController extends Controller
 
     public function add(Request $request)
     {
-        return view('backend.payroll.add');
+        $data['getEmployee'] = User::where('is_role', '=', 0)->get();
+        return view('backend.payroll.add', $data);
     }
 }
