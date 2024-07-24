1dd4027129e00c7caf2140a6bb55765de37af026
kanayofirms <kanyeze@gmail.com>
2024-07-24 07:00:31 +0100

    Create function to insert add payroll record into the db

--- app/Http/Controllers/Backend/PayrollController.php
+++ app/Http/Controllers/Backend/PayrollController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Backend;
 
+use App\Models\PayrollModel;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\ModelsPayrollModel;
@@ -18,5 +19,27 @@ class PayrollController extends Controller
     {
         $data['getEmployee'] = User::where('is_role', '=', 0)->get();
         return view('backend.payroll.add', $data);
+    }
+
+    public function add_post(Request $request)
+    {
+        // dd($request->all());
+        $user = new PayrollModel;
+        $user->employee_id = trim($request->employee_id);
+        $user->number_of_days_worked = trim($request->number_of_days_worked);
+        $user->bonus = trim($request->bonus);
+        $user->overtime = trim($request->overtime);
+        $user->gross_salary = trim($request->gross_salary);
+        $user->cash_advance = trim($request->cash_advance);
+        $user->late_hours = trim($request->late_hours);
+        $user->absent_days = trim($request->absent_days);
+        $user->sss_contribution = trim($request->sss_contribution);
+        $user->insure_health = trim($request->insure_health);
+        $user->total_deduction = trim($request->total_deduction);
+        $user->net_pay = trim($request->net_pay);
+        $user->payroll_monthly = trim($request->payroll_monthly);
+        $user->save();
+
+        return redirect('admin/payroll')->with('success', "Payroll successfully added.");
     }
 }
