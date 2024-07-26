6e5011b61617a3ad04519b1d874d068fcdbb8545
kanayofirms <kanyeze@gmail.com>
2024-07-26 05:49:06 +0100

    Create function to insert updated payroll record into the db

--- app/Http/Controllers/Backend/PayrollController.php
+++ app/Http/Controllers/Backend/PayrollController.php
@@ -35,6 +35,29 @@ class PayrollController extends Controller
         return view('backend.payroll.edit', $data);
     }
 
+    public function edit_update(Request $request, $id)
+    {
+        // dd($request->all());
+
+        $user = PayrollModel::find($id);
+        $user->employee_id = trim($request->employee_id);
+        $user->number_of_days_worked = trim($request->number_of_days_worked);
+        $user->bonus = trim($request->bonus);
+        $user->overtime = trim($request->overtime);
+        $user->gross_salary = trim($request->gross_salary);
+        $user->cash_advance = trim($request->cash_advance);
+        $user->late_hours = trim($request->late_hours);
+        $user->absent_days = trim($request->absent_days);
+        $user->sss_contribution = trim($request->sss_contribution);
+        $user->insure_health = trim($request->insure_health);
+        $user->total_deduction = trim($request->total_deduction);
+        $user->net_pay = trim($request->net_pay);
+        $user->payroll_monthly = trim($request->payroll_monthly);
+        $user->save();
+
+        return redirect('admin/payroll')->with('success', "Payroll successfully updated.");
+    }
+
     public function add_post(Request $request)
     {
         // dd($request->all());
