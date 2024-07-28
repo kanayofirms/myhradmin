54d69c0b023bf78a1f797d5b6145c643b2125174
kanayofirms <kanyeze@gmail.com>
2024-07-28 01:49:53 +0100

    Create function to get position record from the db

---
+++ app/Models/PositionModel.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class PositionModel extends Model
+{
+    use HasFactory;
+
+    protected $table = 'position';
+
+    static public function getRecord($request)
+    {
+        $return = self::select('position.*')
+            ->orderBy('id', 'desc');
+        $return = $return->paginate(20);
+
+        return $return;
+    }
+}
