4dad00a7f8867bfbf3d98fa6efd46368fef59428
kanayofirms <kanyeze@gmail.com>
2024-07-28 01:46:18 +0100

    Create function to insert position record into the db

--- app/Http/Controllers/Backend/PositionController.php
+++ app/Http/Controllers/Backend/PositionController.php
@@ -4,16 +4,37 @@ namespace App\Http\Controllers\Backend;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Models\PositionModel;
 
 class PositionController extends Controller
 {
     public function index(Request $request)
     {
-        return view('backend.position.list');
+        $data['getRecord'] = PositionModel::getRecord($request);
+        return view('backend.position.list', $data);
     }
 
     public function add(Request $request)
     {
         return view('backend.position.add');
+    }
+
+    public function add_post(Request $request)
+    {
+        $user = request()->validate([
+            'position_name' => 'required',
+            'daily_rate' => 'required',
+            'monthly_rate' => 'required',
+            'working_days_per_month' => 'required'
+        ]);
+
+        $user = new PositionModel;
+        $user->position_name = trim($request->position_name);
+        $user->daily_rate = trim($request->daily_rate);
+        $user->monthly_rate = trim($request->monthly_rate);
+        $user->working_days_per_month = trim($request->working_days_per_month);
+        $user->save();
+
+        return redirect('admin/position')->with('success', "Position successfully added.");
     }
 }
