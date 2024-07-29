aa705ae63b79a0a12b9b241b9059a0248a66c111
kanayofirms <kanyeze@gmail.com>
2024-07-29 20:55:54 +0100

    Refactor dashboard function to get employees record and count

--- app/Http/Controllers/Backend/DashboardController.php
+++ app/Http/Controllers/Backend/DashboardController.php
@@ -4,12 +4,14 @@ namespace App\Http\Controllers\Backend;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class DashboardController extends Controller
 {
     public function dashboard(Request $request)
     {
-        return view('backend.dashboard.list');
+        $data['getEmployeesCount'] = User::count();
+        return view('backend.dashboard.list', $data);
     }
 }
 
