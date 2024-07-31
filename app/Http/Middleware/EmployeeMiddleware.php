1ff8631afe0a6b0cbadc9d8f48e11043dd32f610
kanayofirms <kanyeze@gmail.com>
2024-07-31 23:56:20 +0100

    Create file for employee middleware and update app

---
+++ app/Http/Middleware/EmployeeMiddleware.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Support\Facades\Auth;
+
+class EmployeeMiddleware
+{
+    public function handle($request, Closure $next)
+    {
+
+        if (Auth::check()) {
+
+            if (Auth::user()->is_role == 0) {
+                return $next($request);
+            } else {
+                Auth::logout();
+                return redirect(url('/'));
+            }
+        } else {
+            Auth::logout();
+            return redirect(url('/'));
+        }
+    }
+}
+
+
+?>
