32be2ba015dc39ceb96623c82dd4bd7de1c9138f
kanayofirms <kanyeze@gmail.com>
2024-07-31 07:57:58 +0100

    Create total employees by role and redirect link

--- resources/views/backend/dashboard/list.blade.php
+++ resources/views/backend/dashboard/list.blade.php
@@ -62,14 +62,14 @@
                         <!-- small box -->
                         <div class="small-box bg-warning">
                             <div class="inner">
-                                <h3>44</h3>
+                                <h3>{{ !empty($getEMPCount) ? $getEMPCount : '0' }}</h3>
 
-                                <p>User Registrations</p>
+                                <p>Total Employees</p>
                             </div>
                             <div class="icon">
                                 <i class="ion ion-person-add"></i>
                             </div>
-                            <a href="#" class="small-box-footer">More info <i
+                            <a href="{{ url('admin/employees') }}" class="small-box-footer">More info <i
                                     class="fas fa-arrow-circle-right"></i></a>
                         </div>
                     </div>
