829720e9ca00f8bb811e3354e485f863c252cef7
kanayofirms <kanyeze@gmail.com>
2024-07-31 23:08:11 +0100

    Create input for password with validation

--- resources/views/backend/employees/add.blade.php
+++ resources/views/backend/employees/add.blade.php
@@ -63,6 +63,16 @@
                                     </div>
 
                                     <div class="form-group row">
+                                        <label class="col-sm-2 col-form-label">Password
+                                            <span style="color: red;">*</span></label>
+                                        <div class="col-sm-10">
+                                            <input type="password" value="" name="password" class="form-control"
+                                                placeholder="Enter Password" required>
+                                            <span style="color:red;">{{ $errors->first('password') }}</span>
+                                        </div>
+                                    </div>
+
+                                    <div class="form-group row">
                                         <label class="col-sm-2 col-form-label">Phone Number
                                             <span style="color: red;"></span></label>
                                         <div class="col-sm-10">
