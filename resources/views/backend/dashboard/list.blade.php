a428d88914a6cb57f348ed103cdf1374470c2d6f
kanayofirms <kanyeze@gmail.com>
2024-07-31 09:30:33 +0100

    Create yesterday region count

--- resources/views/backend/dashboard/list.blade.php
+++ resources/views/backend/dashboard/list.blade.php
@@ -127,7 +127,7 @@
                         <!-- small box -->
                         <div class="small-box bg-success">
                             <div class="inner">
-                                <h3>{{ !empty($TodayRegion) ? $TodayRegion : '' }}</h3>
+                                <h3>{{ !empty($TodayRegion) ? $TodayRegion : '0' }}</h3>
 
                                 <p>Today Region</p>
                             </div>
@@ -135,6 +135,38 @@
                                 <i class="ion ion-stats-bars"></i>
                             </div>
                             <a href="{{ url('admin/regions') }}" class="small-box-footer">More info <i
+                                    class="fas fa-arrow-circle-right"></i></a>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-3 col-6">
+                        <!-- small box -->
+                        <div class="small-box bg-success">
+                            <div class="inner">
+                                <h3>{{ !empty($YesterdayRegion) ? $YesterdayRegion : '0' }}</h3>
+
+                                <p>Yesterday Region</p>
+                            </div>
+                            <div class="icon">
+                                <i class="ion ion-stats-bars"></i>
+                            </div>
+                            <a href="{{ url('admin/regions') }}" class="small-box-footer">More info <i
+                                    class="fas fa-arrow-circle-right"></i></a>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-3 col-6">
+                        <!-- small box -->
+                        <div class="small-box bg-info">
+                            <div class="inner">
+                                <h3>{{ !empty($getJobHistoryCount) ? $getJobHistoryCount : '' }}</h3>
+
+                                <p>Job History</p>
+                            </div>
+                            <div class="icon">
+                                <i class="ion ion-bag"></i>
+                            </div>
+                            <a href="{{ url('admin/job_history') }}" class="small-box-footer">More info <i
                                     class="fas fa-arrow-circle-right"></i></a>
                         </div>
                     </div>
