732d79295d4b4200c23c0ffa46f1ff2f3c5b9f38
kanayofirms <kanyeze@gmail.com>
2024-07-31 08:21:05 +0100

    Create total regions and redirect link

--- resources/views/backend/dashboard/list.blade.php
+++ resources/views/backend/dashboard/list.blade.php
@@ -106,6 +106,23 @@
                                     class="fas fa-arrow-circle-right"></i></a>
                         </div>
                     </div>
+
+                    <div class="col-lg-3 col-6">
+                        <!-- small box -->
+                        <div class="small-box bg-success">
+                            <div class="inner">
+                                <h3>{{ !empty($getRegionsCount) ? $getRegionsCount : '0' }}</h3>
+
+                                <p>Regions</p>
+                            </div>
+                            <div class="icon">
+                                <i class="ion ion-stats-bars"></i>
+                            </div>
+                            <a href="{{ url('admin/regions') }}" class="small-box-footer">More info <i
+                                    class="fas fa-arrow-circle-right"></i></a>
+                        </div>
+                    </div>
+
                 </div>
                 <!-- /.row -->
                 <!-- Main row -->
