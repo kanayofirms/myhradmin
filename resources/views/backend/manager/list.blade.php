8f34a9ece7c5632df4baa7a470d9d35e18922ea7
kanayofirms <kanyeze@gmail.com>
2024-08-01 16:26:33 +0100

    Refactor code for readability

--- resources/views/backend/manager/list.blade.php
+++ resources/views/backend/manager/list.blade.php
@@ -97,7 +97,7 @@
 
                         <div class="card">
                             <div class="card-header">
-                                <h3 class="card-title">Jobs List</h3>
+                                <h3 class="card-title">Manager List</h3>
                             </div>
 
                             <div class="card-body p-0">
