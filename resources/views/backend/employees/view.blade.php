cd112526f1eaa816f00760f1f4aad6c20becc284
kanayofirms <kanyeze@gmail.com>
2024-08-02 20:51:17 +0100

    Refactor code to fetch interview record and update app

--- resources/views/backend/employees/view.blade.php
+++ resources/views/backend/employees/view.blade.php
@@ -152,6 +152,20 @@
                                     </div>
 
                                     <div class="form-group row">
+                                        <label class="col-sm-2 col-form-label">Interview
+                                            <span style="color: red;"></span></label>
+                                        <div class="col-sm-10">
+                                            @if ($getRecord->interview == '0')
+                                                Cancel
+                                            @elseif ($getRecord->interview == '1')
+                                                Pending
+                                            @elseif ($getRecord->interview == '2')
+                                                Completed
+                                            @endif
+                                        </div>
+                                    </div>
+
+                                    <div class="form-group row">
                                         <label class="col-sm-2 col-form-label">Created Date
                                             <span style="color: red;"></span></label>
                                         <div class="col-sm-10">
