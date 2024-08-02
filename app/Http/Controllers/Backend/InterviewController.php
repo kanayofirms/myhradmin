eb085ca30c9881090fb27266610fa44416dbcee0
kanayofirms <kanyeze@gmail.com>
2024-08-02 20:43:39 +0100

    Refactor interview function to get user record and pass data to view

--- app/Http/Controllers/Backend/InterviewController.php
+++ app/Http/Controllers/Backend/InterviewController.php
@@ -4,11 +4,14 @@ namespace App\Http\Controllers\Backend;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Models\User;
+use Auth;
 
 class InterviewController extends Controller
 {
     public function Interview(Request $request)
     {
-        return view('backend.employee.interview.list');
+        $data['getRecord'] = User::find(Auth::user()->id);
+        return view('backend.employee.interview.list', $data);
     }
 }
