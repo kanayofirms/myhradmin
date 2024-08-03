a44710e24b399b9a38dc39ee396b9073242e0758
kanayofirms <kanyeze@gmail.com>
2024-08-03 11:21:39 +0100

    Create email message

---
+++ resources/views/emails/employees_new_create.blade.php
@@ -0,0 +1,15 @@
+Hello,
+
+<br>
+
+Your Email ID: - <b> {{ $user->email }} </b>
+
+<br>
+
+Your Password: - <b>{{ $user->random_password }}</b>
+
+<br>
+
+Thank You, <br>
+
+{{ config('app.name') }}
